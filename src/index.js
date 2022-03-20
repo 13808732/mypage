e7facf8f1716200d75b258447100ae17373525f9
haotang <haotang08@gmail.com>
2022-03-20 18:56:58 +1100

    add gh-pages使用说明

--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ import { HashRouter as Router } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-// import { createStore, applyMiddleware } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import rootReducer from "./reducers";
-
-// const middleware = [thunk];
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// );
 
 ReactDOM.render(
   <React.StrictMode>
